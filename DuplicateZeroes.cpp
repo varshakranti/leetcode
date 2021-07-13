f1469d93d5addb9f3825d6668eb06e99c0a999a0
Varsha R Kranti <75674655+varshakranti@users.noreply.github.com>
2021-07-13 21:01:17 +0530

    Update DuplicateZeroes.cpp

--- DuplicateZeroes.cpp
+++ DuplicateZeroes.cpp
@@ -3,13 +3,10 @@ public:
     void duplicateZeros(vector<int>& arr) {
         
         int n = arr.size();
-        for(int i=0; i<n; i++){
+        for(int i=n-1; i>=0; i--){
             if(arr[i]==0){
                 arr.insert(arr.begin() + i, 0);
                 arr.pop_back();//to maintain the length of the array
-            }
-            else{
-                
             }
         }
     }

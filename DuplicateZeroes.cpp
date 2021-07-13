cc24ccd2005a968f2e3a262e6a1fea7e3662d38d
Varsha R Kranti <75674655+varshakranti@users.noreply.github.com>
2021-07-13 20:33:51 +0530

    Create DuplicateZeroes.cpp

---
+++ DuplicateZeroes.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    void duplicateZeros(vector<int>& arr) {
+        
+        int n = arr.size();
+        for(int i=0; i<n; i++){
+            if(arr[i]==0){
+                arr.insert(arr.begin() + i, 0);
+                arr.pop_back();//to maintain the length of the array
+            }
+            else{
+                
+            }
+        }
+    }
+};

6490e5b8ff8b718d8ab60af2d0e01bc3750574b8
Varsha R Kranti <75674655+varshakranti@users.noreply.github.com>
2021-07-18 23:21:19 +0530

    Update SortArrayByParity.cpp

--- SortArrayByParity.cpp
+++ SortArrayByParity.cpp
@@ -3,20 +3,22 @@ public:
     vector<int> sortArrayByParity(vector<int>& nums) {
         int n = nums.size();
         int j = 0;
-        // for(int i=0; i<n; i++){
-        //     if(nums[i]%2==0)
-        //     {
-        //         nums[j] = nums[i];
-        //         j++;
-        //     }
-        // }
-        for(int i=0; i<n; i++){
-            if(nums[i]%2!=0)
+        for(int i=0; i<n; ++i){
+            if(nums[i]%2==0)
             {
                 nums[j] = nums[i];
                 j++;
             }
         }
+        
+        for(int i=0; i<n; ++i){
+            if(nums[i]%2==1)
+            {
+                nums[j] = nums[i];
+                j++;
+            }
+        }
+        
         return nums;
     }
 };

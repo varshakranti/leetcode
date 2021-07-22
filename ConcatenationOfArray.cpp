b06cf0aae5bc1b4243c5c81dd55d58e98b82bc61
Varsha R Kranti <75674655+varshakranti@users.noreply.github.com>
2021-07-22 21:51:00 +0530

    Update ConcatenationOfArray.cpp

--- ConcatenationOfArray.cpp
+++ ConcatenationOfArray.cpp
@@ -1,1 +1,23 @@
+class Solution {
+public:
+    vector<int> getConcatenation(vector<int>& nums) {
+        int n = nums.size();
+        vector<int>ans(2*n);
+        for(int i=0; i<n; i++) {
+            ans[i] = nums[i];
+            ans[n+i] = nums[i];
+        }
+        return ans;
+    }
+};
 
+// class Solution {
+// public:
+//     vector<int> getConcatenation(vector<int>& nums) {
+//        vector<int>result(2 * nums.size()); 
+//         for (int i =0; i<nums.size(); i++){
+//             result[i] = result[i + nums.size()] = nums[i];
+//         }
+//         return result;
+//     }
+// };

ac9fedfcd51ece7d03e08ac379715e53e482458a
Varsha R Kranti <75674655+varshakranti@users.noreply.github.com>
2021-07-22 21:03:09 +0530

    Update SearchInsertPosition.cpp

--- SearchInsertPosition.cpp
+++ SearchInsertPosition.cpp
@@ -14,3 +14,26 @@ public:
         return c;
     }
 };
+
+
+//by binary search
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        
+        int low = 0;
+        int high = nums.size()-1;
+        int mid;
+        while(low<=high)
+        {
+            mid = low + (high-low)/2;
+            if(nums[mid]==target)
+                return mid;
+            else if(nums[mid]<target)
+                low = mid+1;
+            else
+                high = mid-1;
+        }
+        return low;
+    }
+};

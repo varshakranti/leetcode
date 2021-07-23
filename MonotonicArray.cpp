73fe24aefdab65140fdda68b99f77b9ce1fa8958
Varsha R Kranti <75674655+varshakranti@users.noreply.github.com>
2021-07-23 19:52:01 +0530

    Update MonotonicArray.cpp

--- MonotonicArray.cpp
+++ MonotonicArray.cpp
@@ -1,1 +1,20 @@
+class Solution {
+public:
+    bool isMonotonic(vector<int>& nums) {
+        int n=nums.size();
+        int inc=0,dec=0;
+        for (int i=0;i<n-1;i++)
+        {
+            if(nums[i]<=nums[i+1])
+            inc++;
+            if(nums[i]>=nums[i+1])
+            dec++;
+        }
+        if(inc==n-1 || dec==n-1)
+        return 1;
+        else
+        return 0; 
+    }
+};
+
 

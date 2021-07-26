2842b595bf435b6b238b68c7fb698b0735833b04
Varsha R Kranti <75674655+varshakranti@users.noreply.github.com>
2021-07-26 20:02:06 +0530

    Update SortArrayByParity2.cpp

--- SortArrayByParity2.cpp
+++ SortArrayByParity2.cpp
@@ -1,1 +1,30 @@
+class Solution {
+public:
+    vector<int> sortArrayByParityII(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> even;
+        vector<int> odd;
+        
+        for(int i =0; i<nums.size(); i++){
+        if(((nums[i])%2)==0){
+            even.push_back(nums[i]);
+        }
+        else{
+            odd.push_back(nums[i]);
+        }
+
+        }
+        int k=0;
+        int i=0;
+        while(k<(n)){
+            nums[k] = even[i];
+            k++;
+            nums[k] = odd[i];
+            k++;
+            i++;
+        }
+        return nums;
+    }
+};
+
 

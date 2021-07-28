751f4dc629b1d8da78bed2eded581f6032b73b14
Varsha R Kranti <75674655+varshakranti@users.noreply.github.com>
2021-07-28 22:42:18 +0530

    Update LengthOfLastWord.cpp

--- LengthOfLastWord.cpp
+++ LengthOfLastWord.cpp
@@ -21,3 +21,26 @@ public:
 
 //trim function didn't work
            
+
+
+
+//First Remove all white space from end then start counting
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int count = 0,space = 0;
+        for(int i =s.length()-1; i>=0;i--){
+            if(s[i]==' ')
+                space++;
+            else
+                break;
+        }
+        for(int i=s.length()-1-space; i>=0; i--){
+            if(s[i] != ' ')
+                count += 1;
+            else
+                return count;
+        }
+        return count;
+    }
+};

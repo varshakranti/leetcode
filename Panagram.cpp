3a82742e94b7f17bae66bec349bfcba87ca9779d
Varsha R Kranti <75674655+varshakranti@users.noreply.github.com>
2021-07-28 23:14:07 +0530

    Update Panagram.cpp

--- Panagram.cpp
+++ Panagram.cpp
@@ -1,1 +1,14 @@
+class Solution {
+public:
+      bool checkIfPangram(string sentence) {
+        unordered_set<char>s;
+        for(int i=0;i<sentence.length();i++){
+            s.insert(sentence[i]);
+        }
+        if(s.size()==26)
+            return true;
+        return false;
+    }
+};
+
 

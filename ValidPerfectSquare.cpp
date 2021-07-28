ad4478cfbca39d09f912664db1f5f8100aab8052
Varsha R Kranti <75674655+varshakranti@users.noreply.github.com>
2021-07-28 23:37:34 +0530

    Update ValidPerfectSquare.cpp

--- ValidPerfectSquare.cpp
+++ ValidPerfectSquare.cpp
@@ -1,1 +1,18 @@
-
+class Solution {
+public:
+    bool isPerfectSquare(int num) {
+        long long int i = 1, j = num, m;
+        
+        while(i<=j){
+        m = (i + j)/2;
+        if( m * m == num)
+            return true;
+        else if (m * m < num){
+            i = m + 1;
+        }
+        else 
+            j = m -1;
+        }
+        return false;
+    }
+};

1732e87b0e6974fd06e3c89dbac3a81f029413e1
Varsha R Kranti <75674655+varshakranti@users.noreply.github.com>
2021-08-01 23:33:24 +0530

    Update ReverseLL.cpp

--- ReverseLL.cpp
+++ ReverseLL.cpp
@@ -1,1 +1,18 @@
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+    ListNode* prevptr=NULL;
+    ListNode* currptr=head;
+    ListNode* nextptr;
 
+    while(currptr!=NULL){
+        nextptr=currptr->next;
+        currptr->next=prevptr;
+
+        prevptr=currptr;
+        currptr=nextptr;
+    }
+    return prevptr;
+        
+    }
+};

8227858e55f6ab1e6faa298a88131d426d8d76e7
Varsha R Kranti <75674655+varshakranti@users.noreply.github.com>
2021-08-03 23:53:58 +0530

    Update MaximumSubarray.cpp

--- MaximumSubarray.cpp
+++ MaximumSubarray.cpp
@@ -1,1 +1,18 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+    int ans=-9999999,cur=0;
+        
+    if(nums.size()==1) 
+        return nums[0];
+        
+    for(int i=0;i<nums.size();i++){
+        cur=cur+nums[i];
+        ans=max(ans,cur);
+        cur=max(0,cur);
+        }
+    return ans;
+    }
+};
+
 

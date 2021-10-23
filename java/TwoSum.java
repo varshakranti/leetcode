6d2419de9cb4dfb50a7e9ba76cba9c9a4dc0ecaa
Varsha R Kranti <75674655+varshakranti@users.noreply.github.com>
2021-10-23 18:43:21 +0530

    Update TwoSum.java

--- java/TwoSum.java
+++ java/TwoSum.java
@@ -1,1 +1,15 @@
-
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int n = nums.length;
+        int a[]=new int[2];
+        for(int i = 0; i<n; i++){
+            for(int j = 0; j<n; j++){
+                if(i!=j && nums[i]+nums[j]==target){
+                    a[0]=i;
+                    a[1]=j;
+                }
+            }
+        }
+        return a;
+    }
+}
